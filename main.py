d2597805ccc0f9a4ee00c868a59c59ca32fd18aa
Attila Racz <racz.attila89@gmail.com>
2017-09-12 14:30:00 +0200

    server handles list_users

--- main.py
+++ main.py
@@ -32,6 +32,12 @@ def route_save_registration():
     return redirect('/')
 
 
+@app.route('/list_users')
+def list_users():
+    queries.listUser()
+    return render_template('all_users.html')
+
+
 @app.route("/question/<id>", methods=["GET"])
 def show_question(id):
     question = queries.showQuestion(id)
